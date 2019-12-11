29cf0de6bf6bf54cfa9657509b411b21632cb6fc
Macjohnan <jo.jomac.mac801@gmail.com>
2019-12-11 17:58:37 +0300

    Adding controllers and views to an MVC project built from an empty template.

---
+++ Csharp-projects/Dotnet-projects/MvcFromEmptyProject/MvcFromEmptyProject/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MvcFromEmptyProject.Controllers {
+
+    public class HomeController : Controller {
+
+        public ActionResult Index() {
+            ViewBag.Title = "Simple MVC App";
+            return View();
+        }
+    }
+}
