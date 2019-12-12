1a51b08286c67f68245a911f8076a16a19561c06
Macjohnan <jo.jomac.mac801@gmail.com>
2019-12-12 10:01:36 +0300

    MVC .NET Core Project

---
+++ Csharp-projects/Dotnetcore-projects/MvcFromEmptyProjectCore/MvcFromEmptyProjectCore/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcFromEmptyProjectCore.Controllers {
+
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+
+            ViewBag.Title = "Core MVC";
+            return View();
+        }
+    }
+}
---
+++ Csharp-projects/Dotnetcore-projects/MvcFromEmptyProjectCore/MvcFromEmptyProjectCore/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MvcFromEmptyProjectCore {
+    public class Startup {
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            } else {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+
+        }
+    }
+}
